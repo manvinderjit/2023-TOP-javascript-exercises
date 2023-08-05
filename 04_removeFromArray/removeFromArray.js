cce5c5dfdfefa78c061905d3a4aed863ed330323
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-05 18:23:00 -0400

    comp: 04-remove-from-array complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args){
+    
+    for(let i=1; i<arguments.length; i++)
+    {
+        for(let j=0; j<array.length; j++){
+            if(arguments[i] === array[j]){
+                array.splice(j, 1);                      
+            }
+        }
+    }
+    return(array);
 };
 
 // Do not edit below this line
