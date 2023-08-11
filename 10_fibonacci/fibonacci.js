415814d994c0089e3c3c3e68c421731c4e838486
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-11 00:51:39 -0400

    comp: 10-fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if(parseInt(position) <0 || isNaN(parseInt(position))) return "OOPS";
+    if(parseInt(position) === 0) return 0;
+    if(parseInt(position) === 1 || parseInt(position) === 2) return 1;
+    
+    let firstNumber = 1;
+    let secondNumber = 1;
+        
+    for (let i = 3; i<position; i++) {
+        let temp = secondNumber;
+        secondNumber += firstNumber;        
+        firstNumber = temp;
+    }
+    return firstNumber + secondNumber;
 };
 
 // Do not edit below this line
