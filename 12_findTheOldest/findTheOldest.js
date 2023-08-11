08b35d4012ea606932198ce2b31aa352a6fbe11d
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-11 01:35:22 -0400

    comp: 12-find-the-oldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+   
+    function getAge(person){
+        return (person['yearOfDeath'] ? person['yearOfDeath']: new Date().getFullYear()) - person['yearOfBirth']
+    };
 
+    array.sort(function (a, b) {      
+
+        return getAge(a) - getAge(b);
+        
+    });
+
+    return array[array.length-1];
 };
 
 // Do not edit below this line
