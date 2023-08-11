652a7b182475e5b78ee5b9944d77959662c4d866
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-10 22:09:07 -0400

    comp: 08-calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a , b) {
+  return a + b;  	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	return inputArray.reduce( (total, num) => {
+    return total + num;    
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {  
+  return inputArray.reduce( (total, num) => {
+    return total * num;    
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);	
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNumber) {
+  inputNumber = parseInt(inputNumber);
+  if(inputNumber === 0 || inputNumber === 1)	return 1;
+  if(inputNumber >= 2){
+    let factorialValue = 1;
+    for(let i=1; i<=inputNumber; i++){
+      factorialValue *= i;
+    }
+    return factorialValue;
+  }
 };
 
 // Do not edit below this line
