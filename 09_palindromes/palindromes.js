b14eb54943372d156417f7c0fd2597c8fe07aa95
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-08-10 22:30:27 -0400

    comp: 09-palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    inputString = (inputString.toLowerCase()).replace(/[^a-z0-9]/g, "");
+    return inputString.split("").reverse().join("") == inputString;1    
 };
 
 // Do not edit below this line
